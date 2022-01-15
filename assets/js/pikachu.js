0255c77bf5a90688fc01b0272fcc8bdb7c5e05f6
tuyen-cpu <tranquangtuyen326326@gmail.com>
2022-01-15 23:57:52 +0700

    add json,js, exchange icon, search icon

---
+++ assets/js/pikachu.js
@@ -0,0 +1,32 @@
+export default class Pikachu {
+    constructor(game, x, y, value) {
+        this.game = game;
+        this.x = x;
+        this.y = y;
+        this.value = value;
+        this.init();
+        this.isSelected = false;
+
+    }
+    init() {
+
+        this.cell = document.createElement("td");
+        this.cell.className = "pikachu";
+
+        if (this.value != 0) {
+            let img = document.createElement("img");
+            img.src = "./assets/img/a" + this.value + ".png";
+            this.cell.onclick = () => this.game.check(this.cell, this.x, this.y)
+            this.cell.appendChild(img);
+        }
+
+    }
+    set setSelected(para) {
+        this.isSelected = para;
+    }
+    get getElement() {
+        return this.cell;
+    }
+
+
+}
